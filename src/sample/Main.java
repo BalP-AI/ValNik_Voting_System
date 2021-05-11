30467806f9a448e2bca1e1ecb55d580bd4878ca4
balantis <balantispat@gmail.com>
2021-05-11 13:23:53 +0300

    first window updated still learning

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../FXMLS/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("ValNik Voting System");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
